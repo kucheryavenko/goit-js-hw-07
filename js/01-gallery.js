3b8dcbbffd1526f7f7aff51d1bb7be64d3d7d166
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-07-04 14:44:16 +0300

    task-01-addedMarkup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+// console.log(galleryContainer);
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+ 
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+     <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join('');
+}; 
