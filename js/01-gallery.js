10b6c861b5097989f6cd2a0c2306dc4cc3553fba
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-07-04 20:22:41 +0300

    task-01-done
    library basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryContainer = document.querySelector('.gallery');
-// console.log(galleryContainer);
 const cardsMarkup = createImageCardsMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
- 
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup); // Рендерим элементы разметки  
+galleryContainer.addEventListener('click', onOpenCloseModal); // Открываем модальное окно (изображение)
+
+// Функция createImageCardsMarkup создаёт разметку по массиву данных galleryItems
 function createImageCardsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
      <div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
+        <a class="gallery__link" href="${original}">
             <img
             class="gallery__image"
             src="${preview}"
@@ -25,3 +24,26 @@ function createImageCardsMarkup(galleryItems) {
     })
     .join('');
 }; 
+
+// Функция onOpenCloseModal открывает/закрывает модальное окно (изображение) с помощью библиотеки basicLightbox
+function onOpenCloseModal(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" />
+    `, {
+        onShow: () => {window.addEventListener('keydown', onEscKeyPress)}
+    });
+
+    instance.show();
+
+    function onEscKeyPress(event) {
+        if (event.code === 'Escape') {
+            instance.close();
+        };
+    };
+};
